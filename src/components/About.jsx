f7367336adcb30c252a1844da5b7f0d8a2b891bd
ifan <ifanabedin034@gmail.com>
2024-12-01 18:54:19 +0600

    Add my code

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+      About
+    </div>
+  )
+}
+
+export default About
